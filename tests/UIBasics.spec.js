6048a35ea1451232241476181e6f86f5e12fe134
PankajMahaldar <Pankaj.mahaldar@outlook.com>
2024-08-21 16:43:18 +0530

    test.Only is removed from codes

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -1,6 +1,6 @@
 const {test, expect} = require('@playwright/test');
 
-test.only('Browser_Context_Automation',async ({browser}) =>
+test('Browser_Context_Automation',async ({browser}) =>
 {
     const context= await browser.newContext();
     const page = await context.newPage();
